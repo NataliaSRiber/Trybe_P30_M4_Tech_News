f19a880a0f0d1a639c13458aa8b006471725dfcc
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-04-17 17:12:21 -0300

    feat: requirement 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -36,6 +36,41 @@ def scrape_next_page_link(html_content):
 # Requisito 4
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css("link[rel='canonical']::attr(href)").get()
+    title = selector.css("#js-article-title::text").get()
+    timestamp = selector.css("#js-article-date::attr(datetime)").get()
+    writer = selector.css(".z--font-bold *::text").get().strip()
+    comments_count = selector.css("#js-comments-btn::attr(data-count)").get()
+    comments_count = 0 if comments_count is None else int(comments_count)
+    shares_count = selector.css(
+        "div.tec--toolbar__item::text"
+    ).re_first(r"[0-9][0-9]")
+    shares_count = 0 if shares_count is None else shares_count
+    summary = "".join(selector.css(
+        "div.tec--article__body p:first-child *::text").getall())
+    sources = [
+        source[1:-1]
+        for source in selector.css("div.z--mb-16 a::text").getall()
+    ]
+    categories = [
+        category[1:-1]
+        for category in selector.css("#js-categories a::text").getall()
+    ]
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": shares_count,
+        "comments_count": comments_count,
+        "summary": summary,
+        "sources": sources,
+        "categories": categories,
+    }
+
+
+# scrape_noticia(fetch(url1))
 
 
 # Requisito 5
