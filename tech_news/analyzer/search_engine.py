fa932a0f89d6f2cf30ceda432c7d9880dbaa4e28
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-04-18 20:40:18 -0300

    feat: adds requirement 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+import datetime
 
 
 # Requisito 6
@@ -11,7 +12,12 @@ def search_by_title(title):
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
-
+    try:
+        datetime.date.fromisoformat(date)
+        per_date_list = search_news({"timestamp": {"$regex": date}})
+        return [(news["title"], news["url"]) for news in per_date_list]
+    except ValueError:
+        raise ValueError("Data inválida")
 
 # Requisito 8
 def search_by_source(source):
