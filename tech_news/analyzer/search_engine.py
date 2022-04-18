3a945db340d8bbdbd1c22c03e9a06e5fcd8272c6
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-04-18 20:48:10 -0300

    feat: adds requirement 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -30,3 +30,5 @@ def search_by_source(source):
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+    list = search_news({"categories": {"$regex": category, "$options": "i"}})
+    return [(news["title"], news["url"]) for news in list]
